bebc457345b25747cdc244bee845113ad8d8aab6
lstanislao <stanislaoluis@gmail.com>
2024-04-11 10:50:29 -0400

    feat: improve fetch wrapper

--- apps/client/src/services/user/queries.ts
+++ apps/client/src/services/user/queries.ts
@@ -1,17 +1,13 @@
-import { getFetch } from '@/lib/api';
+import { api } from '@/lib/api';
 import { z } from 'zod';
 
 export async function getUsers(options?: RequestInit) {
   const schemaExample = z.object({});
   // remember use zod definition
-  try {
-    const { data, response } = await getFetch({
-      url: '/users',
-      options,
-    });
+  const { data, response } = await api.get({
+    url: '/users',
+    options,
+  });
 
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
+  return data;
 }
