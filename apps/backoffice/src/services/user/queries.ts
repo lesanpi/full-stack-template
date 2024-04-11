ba3c5af53d792fab6fede35dbfc0596d5d9d63e3
lstanislao <stanislaoluis@gmail.com>
2024-04-11 11:31:37 -0400

    fix: hotfix

--- apps/backoffice/src/services/user/queries.ts
+++ apps/backoffice/src/services/user/queries.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
-import { getFetch } from '@/lib/api';
+import { api } from '@/lib/api';
 
 export async function getUsers(options?: RequestInit) {
   // const schemaExample = z.object({});
   // remember use zod definition to validate the response
   try {
-    const { data, response } = await getFetch({
+    const { data, response } = await api.get({
       url: '/users',
       options,
       // schema: schemaExample,
