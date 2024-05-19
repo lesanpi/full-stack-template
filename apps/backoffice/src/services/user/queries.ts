d9f5cf761526e86efe64c49d2660fcda93a541d9
lstanislao <stanislaoluis@gmail.com>
2024-05-19 16:46:46 -0400

    feat: npm run test script

---
+++ apps/backoffice/src/services/user/queries.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+import { api } from '@/lib/api';
+
+export async function getUsers(options?: RequestInit) {
+  // const schemaExample = z.object({});
+  // remember use zod definition to validate the response
+  try {
+    const { data, response } = await api.get({
+      url: '/users',
+      options,
+      // schema: schemaExample,
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
